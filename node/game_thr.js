be26d6fe554f8a582e00fa7e9d59203ad2b7ccc2
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-09-02 18:32:55 +0200

    Second iteration, made console print stars according to rating from JSON parsed episodes file

--- node/game_thr.js
+++ node/game_thr.js
@@ -1,1 +1,19 @@
-game_thr.js
+var fs = require("fs");
+
+function fileactions(err, file) {
+    if (err) {
+        throw err;
+    }
+    var episodes = JSON.parse(file)
+    for (i = 0; i < episodes.length; i++) {
+        customPrint(episodes[i])
+    }
+}
+
+fs.readFile("./episodes.txt", 'utf8', fileactions)
+
+function customPrint(episode) {
+    var stars = Array(Math.round(episode.rating)).join("*")
+    console.log("Title: " + episode.title + "\n" + episode.description + "\n" + "Rating: " + episode.rating + " " + stars)
+
+}
