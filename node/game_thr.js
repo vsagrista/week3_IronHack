eaee7a6911c1a85d8892d24b401f5ee778fec33b
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-09-02 18:53:18 +0200

    Second Iteration: sorted by episodes

--- node/game_thr.js
+++ node/game_thr.js
@@ -5,15 +5,22 @@ function fileactions(err, file) {
         throw err;
     }
     var episodes = JSON.parse(file)
-    for (i = 0; i < episodes.length; i++) {
-        customPrint(episodes[i])
+    sorted_episodes = sortEpisodes(episodes)
+
+    for (i = 0; i < sorted_episodes.length; i++) {
+        customPrint(sorted_episodes[i])
     }
 }
 
 fs.readFile("./episodes.txt", 'utf8', fileactions)
 
-function customPrint(episode) {
+function customPrint(episode) { // prints the name, plot, etc... plus several stars according to the rating. 
     var stars = Array(Math.round(episode.rating)).join("*")
-    console.log("Title: " + episode.title + "\n" + episode.description + "\n" + "Rating: " + episode.rating + " " + stars)
-
+    console.log("Title: " + episode.title + "\n" + episode.description + "\n" + "Rating: " + episode.rating + " " + stars + "\n" + "Episode Number: " + episode.episode_number)
+}
+function sortEpisodes(episodes) {
+	sorted_episodes = episodes.sort(function(a,b){
+		return a.episode_number - b.episode_number}
+		)
+	return sorted_episodes
 }
